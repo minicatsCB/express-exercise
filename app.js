143fd57f948589e8e567f43eb786412b7c26b9ae
Maribel <minicatsCB@users.noreply.github.com>
2019-05-09 20:48:13 +0200

    Serve index static file

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+let options = {
+    index: "index.html"
+};
+app.use(express.static("public", options));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
